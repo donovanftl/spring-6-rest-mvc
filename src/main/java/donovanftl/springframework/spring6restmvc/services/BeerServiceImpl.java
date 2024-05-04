ee6ee729fbdec5a0e5fe5c3dae3715cdca8d2ea8
donovanftl <donovan_torreslan@outlook.com>
2024-05-04 11:23:08 -0600

    4-lombok-logging

--- src/main/java/donovanftl/springframework/spring6restmvc/services/BeerServiceImpl.java
+++ src/main/java/donovanftl/springframework/spring6restmvc/services/BeerServiceImpl.java
@@ -2,20 +2,27 @@ package donovanftl.springframework.spring6restmvc.services;
 
 import donovanftl.springframework.spring6restmvc.entities.Beer;
 import donovanftl.springframework.spring6restmvc.entities.BeerStyle;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Slf4j
+@Service
 public class BeerServiceImpl implements BeerService {
     @Override
     public Beer getBeerById(UUID id) {
+
+        log.debug("Get Beer by Id - in service. Id: " + id.toString());
         return Beer.builder()
                 .id(id)
                 .version(1)
                 .beerName("Galaxy Cat")
                 .beerStyle(BeerStyle.PALE_ALE)
-                .price(new BigDecimal(12.99))
+                .upc("12356")
+                .price(new BigDecimal("12.99"))
                 .quantityOnHand(122)
                 .createdDate(LocalDateTime.now())
                 .updateDate(LocalDateTime.now())
