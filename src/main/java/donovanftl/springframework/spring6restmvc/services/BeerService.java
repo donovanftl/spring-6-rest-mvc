caa53fa69de39088364b7fc20752e3093e785c95
donovanftl <donovan_torreslan@outlook.com>
2024-05-04 11:50:24 -0600

    5-list-beer

--- src/main/java/donovanftl/springframework/spring6restmvc/services/BeerService.java
+++ src/main/java/donovanftl/springframework/spring6restmvc/services/BeerService.java
@@ -2,9 +2,12 @@ package donovanftl.springframework.spring6restmvc.services;
 
 import donovanftl.springframework.spring6restmvc.entities.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
+
+    List<Beer> listBeers();
 
     Beer getBeerById(UUID id);
 }
