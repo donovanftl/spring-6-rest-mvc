36ec994c5eb50b9f73d025820a00504e1737e5af
donovanftl <donovan_torreslan@outlook.com>
2024-05-06 10:05:24 -0600

    18-mock-mvc-config

--- src/main/java/donovanftl/springframework/spring6restmvc/controller/BeerController.java
+++ src/main/java/donovanftl/springframework/spring6restmvc/controller/BeerController.java
@@ -3,6 +3,7 @@ package donovanftl.springframework.spring6restmvc.controller;
 import donovanftl.springframework.spring6restmvc.entities.Beer;
 import donovanftl.springframework.spring6restmvc.services.BeerService;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -13,7 +14,7 @@ import java.util.List;
 import java.util.UUID;
 
 @Slf4j
-@AllArgsConstructor
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/beer")
 public class BeerController {
